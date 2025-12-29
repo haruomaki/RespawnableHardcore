390d09fe73240f0c271d4f54db536bf7788f72b4
Haruo <haruo-mtok@outlook.com>
2025-12-29 17:58:43 +0900

    フォーマッタの設定（インデント・自動改行）調整

--- src/main/java/com/example/respawnable_hardcore/RespawnableHardcore.java
+++ src/main/java/com/example/respawnable_hardcore/RespawnableHardcore.java
@@ -56,7 +56,7 @@ public class RespawnableHardcore {
 
     // Creates a creative tab with the id "examplemod:example_tab" for the example item, that is placed after the combat tab
     public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
-            .title(Component.translatable("itemGroup.examplemod")) //The language key for the title of your CreativeModeTab
+            .title(Component.translatable("itemGroup.examplemod")) // The language key for the title of your CreativeModeTab
             .withTabsBefore(CreativeModeTabs.COMBAT)
             .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
             .displayItems((parameters, output) -> {
